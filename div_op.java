d827aa8eb722b2daf1fe59e9b27ccf0f4923727f
sarveshashelar <70604593+sarveshashelar@users.noreply.github.com>
2020-09-02 21:03:13 +0530

    Add files via upload

---
+++ div_op.java
@@ -0,0 +1,16 @@
+
+public class div_op {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=20,num2=10;
+		int result;
+		result=num1/num2;
+		System.out.println("Result of division operater\t" + result);
+		int result1;
+		result1=num1%num2;
+		System.out.println("Result of % operater\t" + result);
+
+	}
+
+}
